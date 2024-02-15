640f1f6fe4b1489b785888e9de3ef9e9bb4f064f
xCyrik <jorgesancrod@gmail.com>
2024-02-15 09:28:41 -0600

    Semana 3 markdown

--- Laboratorio 1.R
+++ Laboratorio 1.R
@@ -11,3 +11,9 @@ gastos <- c(celular, transporte, gimnasio, comestibles, alquiler, otros)
 sum(gastos) * 5
 sum (gastos) * 10
 barplot(sort(gastos, decreasing = TRUE))
+
+#Texto en markdown
+
+## Esta semana se trabajo con datos para poder combinarlos de distintas 
+## maneras estableciendo variables y trabajando con ellos.
+## La principal forma de trabajarlos en esta ocasión fue  graficandolos
